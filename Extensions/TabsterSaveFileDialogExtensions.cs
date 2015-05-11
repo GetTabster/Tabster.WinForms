d155a544f5b6ea2fbb7110355b835a6dafa52ea3
Nate Shoffner <nate.shoffner@gmail.com>
2015-05-11 13:53:08 -0400

    Renamed Tabster.Controls to Tabster.WinForms

---
+++ Extensions/TabsterSaveFileDialogExtensions.cs
@@ -0,0 +1,58 @@
+#region
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using Tabster.Data;
+using Tabster.Data.Processing;
+
+#endregion
+
+namespace Tabster.WinForms.Extensions
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class TabsterSaveFileDialogExtensions
+    {
+        public static void SetTabsterFilter(this SaveFileDialog saveFileDialog, IEnumerable<ITablatureFileExporter> exporters, bool overwriteFilter = false)
+        {
+            var filterStringBuilder = new StringBuilder();
+
+            var fileTypes = new List<FileType>();
+
+            foreach (var exporter in exporters)
+            {
+                if (exporter.FileType != null && exporter.FileType.Extension != null)
+                {
+                    fileTypes.Add(exporter.FileType);
+                }
+            }
+
+            for (var i = 0; i < fileTypes.Count; i++)
+            {
+                var fileType = fileTypes[i];
+
+                filterStringBuilder.AppendFormat("{0} (*{1})|*{1}", fileType.Name, fileType.Extensions[0]);
+
+                if (i + 1 < fileTypes.Count)
+                    filterStringBuilder.Append("|");
+            }
+
+            if (filterStringBuilder.Length > 0)
+            {
+                if (overwriteFilter)
+                {
+                    saveFileDialog.Filter = filterStringBuilder.ToString();
+                }
+
+                else
+                {
+                    if (saveFileDialog.Filter.Length > 0)
+                        filterStringBuilder.Insert(0, "|");
+
+                    saveFileDialog.Filter += filterStringBuilder.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ TablatureDisplayFont.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using System.Drawing;
+
+#endregion
+
+namespace Tabster.WinForms
+{
+    public static class TablatureDisplayFont
+    {
+        public static Font GetFont()
+        {
+            return new Font("Courier New", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        }
+    }
+}
